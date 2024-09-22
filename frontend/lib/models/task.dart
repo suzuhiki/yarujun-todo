c7ab35eef75d9c151017169671867b2b7dfb982d
Suzuhiki <82814541+suzuhiki@users.noreply.github.com>
2024-09-22 15:31:04 +0900

    [FE]taskが完了したというapi responseの受取

--- frontend/lib/models/task.dart
+++ frontend/lib/models/task.dart
@@ -3,28 +3,33 @@ class Task {
     required this.id,
     required this.title,
     required this.deadline,
-    this.waitlistNum = -1,
+    required this.waitlistNum,
+    required this.done,
   });
 
   final String id;
   final String title;
   final String deadline;
   final int waitlistNum;
+  final bool done;
 
   factory Task.fromJson(dynamic json) {
+    print(json);
     if (json["Waitlist_num"] == "") {
       return Task(
         id: json['ID'] as String,
         title: json['Title'] as String,
         deadline: json['Deadline'] as String,
         waitlistNum: -1,
+        done: json['Done'] as bool,
       );
     } else {
       return Task(
-        id: json['ID'] as String,
+        id: json['Id'] as String,
         title: json['Title'] as String,
         deadline: json['Deadline'] as String,
         waitlistNum: int.parse(json['Waitlist_num'] as String),
+        done: json['Done'] as bool,
       );
     }
   }
